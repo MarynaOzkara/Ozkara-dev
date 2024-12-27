3f90cc7df6ff797f6cda18588382a80874b3f25b
MarynaOzkara <ozkaramaryna@gmail.com>
2024-12-27 22:30:19 +0200

    add mobile menu toggle

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-// Aos.init();
+const nav = document.querySelector(".nav");
+const navBtn = document.querySelector(".mob-nav-btn");
+const navBtnImg = document.querySelector(".nav-btn-img");
+
+navBtn.addEventListener("click", function () {
+  if (nav.classList.toggle("open")) {
+    document.body.classList.add("no_scroll");
+    navBtnImg.src = "/img/nav-close.svg";
+  } else {
+    navBtnImg.src = "/img/nav.svg";
+    document.body.classList.remove("no_scroll");
+  }
+});
